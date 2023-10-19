dd9849b516bbf27e3f7a19b7226fc3d14173b997
jpVinnie <janpaul.ramos@protonmail.com>
2023-10-18 20:45:26 -0400

    WIP

--- grass/syscall.c
+++ grass/syscall.c
@@ -11,37 +11,50 @@
 #include "syscall.h"
 #include <string.h>
 
-static struct syscall *sc = (struct syscall*)SYSCALL_ARG;
+static struct syscall *sc = (struct syscall *)SYSCALL_ARG;
 
-static void sys_invoke() {
-    /* The standard way of system call is using the `ecall` instruction; 
+static void sys_invoke()
+{
+    /* The standard way of system call is using the `ecall` instruction;
      * Switching to ecall is given to students as an exercise */
-    *((int*)0x2000000) = 1;
-    while (sc->type != SYS_UNUSED);
+    // *((int *)0x2000000) = 1;
+    asm("ecall");
+    // This while loop was necessary before because it meant that this function did not return
+    // until the system call had been processed.
+    // Now, it is not necessary because the ecall assembly instruction ensures that this function
+    // will transfer control to the kernel.
+    // while (sc->type != SYS_UNUSED)
+    //     ;
 }
 
-int sys_send(int receiver, char* msg, int size) {
-    if (size > SYSCALL_MSG_LEN) return -1;
+int sys_send(int receiver, char *msg, int size)
+{
+    if (size > SYSCALL_MSG_LEN)
+        return -1;
 
     sc->type = SYS_SEND;
     sc->msg.receiver = receiver;
     memcpy(sc->msg.content, msg, size);
     sys_invoke();
-    return sc->retval;    
+    return sc->retval;
 }
 
-int sys_recv(int* sender, char* buf, int size) {
-    if (size > SYSCALL_MSG_LEN) return -1;
+int sys_recv(int *sender, char *buf, int size)
+{
+    if (size > SYSCALL_MSG_LEN)
+        return -1;
 
     sc->type = SYS_RECV;
     sys_invoke();
     memcpy(buf, sc->msg.content, size);
-    if (sender) *sender = sc->msg.sender;
+    if (sender)
+        *sender = sc->msg.sender;
     return sc->retval;
 }
 
-void sys_exit(int status) {
+void sys_exit(int status)
+{
     struct proc_request req;
     req.type = PROC_EXIT;
-    sys_send(GPID_PROCESS, (void*)&req, sizeof(req));
+    sys_send(GPID_PROCESS, (void *)&req, sizeof(req));
 }
